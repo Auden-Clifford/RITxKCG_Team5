d2454497cd54bd85b583b3be1216c06a4e32d8b6
mohit-kumar-singh55 <mohitkumarsingh12344@gmail.com>
2026-04-16 18:56:37 +0900

    Update: Fixed attack system

--- Punch-Kun's Revenge/Assets/Scripts/Player/PlayerCollisions.cs
+++ Punch-Kun's Revenge/Assets/Scripts/Player/PlayerCollisions.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class PlayerCollisions : MonoBehaviour
 {
+    private PlayerController _player;
+
+    void Start()
+    {
+        _player = PlayerController.Instance;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         // TODO: need to add a check whether player is attacking or not, then only destroy other things
@@ -12,6 +19,17 @@ public class PlayerCollisions : MonoBehaviour
 
             // also giving damage to player
             if (TryGetComponent(out PlayerHealth health)) health.TakeDamage(1);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!_player.IsAttacking) return;
+
+        if (other.gameObject.TryGetComponent(out Health health))
+        {
+            health.TakeDamage(_player.DamageOnAttack);
+            CameraController.Instance.Shake(1f);
         }
     }
 }
